882384805d746f355766d2ab1d4a5aca12c73871
Aneesh165 <aneeshmp165@gmail.com>
2024-09-28 21:54:45 +0530

    28-9-24

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,58 +1,81 @@
-
-import { hightlightsSlides } from "../constants"
-import { useEffect, useRef, useState } from "react"
-
+import { hightlightsSlides } from "../constants";
+import { useEffect, useRef, useState } from "react";
 
 const VideoCarousel = () => {
-  
-    const videoRef = useRef([]);
-    const videoSpanRef = useRef([]);
-    const videoDivRef = useRef([]);   
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
 
-    const [video, setVideo] = useState({
-        isEnd: false,
-        startPlay: false,
-        videoId: 0,
-        isLastVideo: false,
-        isPlaying: false,
-    })
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
 
-    const { isEnd, isLastVideo, startPlay, videoId, isPlaying} = video;
+  const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video;
 
-    useEffect(()=>{
-        const currentProgress = 0;
-        let span = videoSpanRef.current;
+  const [loadedData, setloadedData] = useState([]);
 
-        if(span[videoId]){
-            
-        }
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData]);
 
-    },[videoId, startPlay])
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRef.current;
+
+    if (span[videoId]) {
+      let anim = gsap.to(span[videoId], {
+        onUpdate: () => {},
+        onComplete: () => {},
+      });
+    }
+  }, [videoId, startPlay]);
 
   return (
     <>
-    <div className="flex items-center">
-       {hightlightsSlides.map((list, i)=>(
-        <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, i) => (
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
-                <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                    <video id="video" playsInline={true} preload="auto" muted>
-                        <source src={list.video} type="video/mp4"/>
-                    </video>
-                </div>
-                <div className="absolute top-12 left-[5%] z-10">
-                    {list.textLists.map((text)=>(
-                        <p key={text} className="md:text-2xl text-xl font-medium">
-                            {text}
-                        </p>
-                    ))}
-                </div>
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video
+                  id="video"
+                  playsInline={true}
+                  preload="auto"
+                  muted
+                  ref={(el) => (videoRef.current[i] = el)}
+                  onPlay={() => {
+                    setVideo((prevVideo) => ({
+                      ...prevVideo,
+                      isPlaying: true,
+                    }));
+                  }}
+                >
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                {list.textLists.map((text) => (
+                  <p key={text} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
             </div>
-        </div>
-       ))} 
-    </div>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default VideoCarousel
+export default VideoCarousel;
