b3a26ed8a881cb58fadef1a944d772eb43fd1c7c
Aneesh165 <aneeshmp165@gmail.com>
2024-09-29 14:46:54 +0530

    29-9-24

---
+++ src/components/ChipSet.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ChipSet = () => {
+  return (
+    <section className='common-padding'>
+        <div className='screen-max-width'>
+            <div id='chip'>
+              
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default ChipSet
